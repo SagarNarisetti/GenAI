ea995ca86570e3da848435b5bac69b33bdd2c0a0
Sagar Narisetti <sagarnvs724@gmail.com>
2026-05-27 20:26:29 +0100

    default variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,4 @@
+### access
 variable "aws_region" {
     description = "AWS region"
     type = string
@@ -26,4 +27,34 @@ variable "iam_policy_name" {
     description = "IAM policy name"
     type = string
     default = "MLEngDevPolicy"
+}
+
+variable "vpc_cidr" {
+    description = "CIDR Ip address"
+    type = string
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+    description = "public subnet IP"
+    type = string
+    default = "10.0.1.0/16"
+}
+
+variable "private_data_subnet" {
+    description = "private data subnet IP"
+    type = string
+    default = "10.0.2.0/24"
+}
+
+variable "private_app_subnet" {
+    description = "private app subnet IP"
+    type = string
+    default = "10.0.3.0/24"
+}
+
+variable "private_ml_subnet" {
+    description = "private ml subnet IP"
+    type = string
+    default = "10.0.4.0/24"
 }
