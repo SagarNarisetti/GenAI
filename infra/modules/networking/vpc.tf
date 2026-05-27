490e778d99f8d6c67814ced4d216097bb2a95910
Sagar Narisetti <sagarnvs724@gmail.com>
2026-05-28 00:30:53 +0100

    VPC configureation update

--- infra/modules/networking/vpc.tf
+++ infra/modules/networking/vpc.tf
@@ -1,3 +1,21 @@
+### VPC
 resource "aws_vpc" "main" {
     cidr_block = var.vpc_cidr
+    enable_dns_hostnames = true
+    enable_dns_support = true
+
+    tags={
+        Name = "MlengProject"
+        Environment = var.environment
+    }
+}
+
+### Interned gateway
+resource "aws_internet_gateway" "main" {
+    vpc_id = aws_vps.main.id
+
+    tags = {
+        Name = "MlengProject-igw"
+        Environment = var.environment
+    }
 }
