eb55a339355318ed883c92bcb6f33957f2290ce2
Sagar Narisetti <sagarnvs724@gmail.com>
2026-05-27 20:26:14 +0100

    root outputs includes access keys, iam user, role, policy outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,13 +1,3 @@
-output "iam_user_arn" {
-  description = "ARN of IAM user"
-  value       = module.iam.user_arn
-}
-
-output "iam_user_name" {
-  description = "Name of IAM user"
-  value       = module.iam.user_name
-}
-
 output "access_key_id" {
   description = "Access key ID for the IAM user"
   value       = module.iam.access_key_id
@@ -20,12 +10,32 @@ output "secret_access_key" {
   sensitive   = true
 }
 
+output "iam_user_arn" {
+  description = "ARN of IAM user"
+  value = module.iam.user_arn
+}
+
+output "iam_user_name" {
+  description = "Name of IAM user"
+  value = module.iam.user_name
+}
+
 output "iam_role_arn" {
   description = "ARN of IAM role"
-  value       = module.iam.role_arn
+  value = module.iam.role_arn
 }
 
 output "iam_role_name" {
   description = "Name of IAM role"
-  value       = module.iam.role_name
+  value = module.iam.role_name
+}
+
+output "iam_policy_arn" {
+  description = "ARN of IAM policy"
+  value = module.iam.policy_arn
+}
+
+output "iam_policy_name" {
+  description = "Name of IAM policy"
+  value = module.iam.policy_name
 }
