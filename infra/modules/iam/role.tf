8ac7ca011352d0d63c519c13151dcf69d86471fa
Sagar Narisetti <sagarnvs724@gmail.com>
2026-06-02 22:48:05 +0100

    added additional permission to the role

--- infra/modules/iam/role.tf
+++ infra/modules/iam/role.tf
@@ -2,14 +2,31 @@ resource "aws_iam_role" "mleng_role" {
     name = var.iam_role_name
     assume_role_policy = jsonencode({
         Version = "2012-10-17"
-        Statement = [{
-            Sid = "AllowUserAssumption"
-            Action = "sts:AssumeRole"
-            Effect = "Allow"
-            Principal = {
-                AWS = [aws_iam_user.mleng_user.arn]
+        Statement = [
+            {
+                Sid = "AllowUserAssumption"
+                Action = "sts:AssumeRole"
+                Effect = "Allow"
+                Principal = {
+                    AWS = [
+                        aws_iam_user.mleng_user.arn
+                        ]
+                        }
+            },
+            {
+                Sid    = "AllowServiceAssumption"
+                Effect = "Allow"
+                Principal = {
+                Service = [
+                    "sagemaker.amazonaws.com",
+                    "lambda.amazonaws.com",
+                    "codebuild.amazonaws.com",
+                    "codepipeline.amazonaws.com"
+                ]
                 }
-        }]
+                Action = "sts:AssumeRole"
+            }
+            ]
     })
     tags = {
         Environment = var.environment
