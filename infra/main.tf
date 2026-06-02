66e8aa4782dfb8ebc36b94ff42200e2c0cf6900f
Sagar Narisetti <sagarnvs724@gmail.com>
2026-06-02 22:46:42 +0100

    added networking to use VPC and subnets

--- infra/main.tf
+++ infra/main.tf
@@ -20,3 +20,23 @@ module "iam" {
     iam_policy_name = var.iam_policy_name
     environment = var.environment
 }
+
+module "networking" {
+    source = "./modules/networking"
+
+    vpc_cidr            = var.vpc_cidr
+    public_subnet_cidr  = var.public_subnet_cidr
+    private_data_subnet = var.private_data_subnet
+    private_app_subnet  = var.private_app_subnet
+    private_ml_subnet   = var.private_ml_subnet
+
+    environment = var.environment
+
+    public_subnet_az = var.public_subnet_az
+    private_data_subnet_az = var.private_data_subnet_az
+    private_app_subnet_az = var.private_app_subnet_az
+    private_ml_subnet_az = var.private_ml_subnet_az
+
+    ## explicit dependency 
+    depends_on = [module.iam]
+}
