a0a34063f442973ddd752a8a8d31454e24af1c33
Sagar Narisetti <sagarnvs724@gmail.com>
2026-06-02 22:49:38 +0100

    updated networking details

--- infra/modules/networking/varaibles.tf
+++ infra/modules/networking/varaibles.tf
@@ -1,43 +1,60 @@
 variable "environment" {
     description = "env region"
     type = string
+    default = "prod"
+}
+## VPC 
+variable "vpc_cidr" {
+    description = "CIDR Ip address"
+    type = string
+    default = "10.0.0.0/16"
+}
+### subnets
+variable "public_subnet_cidr" {
+    description = "public subnet IP"
+    type = string
+    default = "10.0.1.0/24"
 }
 
 variable "private_data_subnet" {
     description = "private data subnet IP"
     type = string
+    default = "10.0.2.0/24"
 }
 
 variable "private_app_subnet" {
     description = "private app subnet IP"
     type = string
+    default = "10.0.3.0/24"
 }
 
 variable "private_ml_subnet" {
     description = "private ml subnet IP"
     type = string
+    default = "10.0.4.0/24"
 }
 
-variable "vpc_cidr" {
-    description = "CIDR Ip address"
+
+### availability zones 
+variable "public_subnet_az" {
+    description = "public subnet AZ"
     type = string
+    default = "eu-west-1a"
 }
 
-variable "public_subnet_cidr" {
-    description = "public subnet IP"
-    type = string
-}
-
-variables "private_data_subnet_az" {
+variable "private_data_subnet_az" {
     description = "private data subnet AZ"
     type = string
+    default = "eu-west-1a"
 }
- variables "private_app_subnet" {
+ variable "private_app_subnet_az" {
     description = "private app subnet AZ"
     type = string
+    default = "eu-west-1b"
  }
 
- variables "private_ml_subnet_az" {
+ variable "private_ml_subnet_az" {
     description = "private ml subnet AZ"
     type = string
+    default = "eu-west-1c"
  }
