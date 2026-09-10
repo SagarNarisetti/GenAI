2f64de5a84bb441151b17ab3597458248086be53
Sagar Narisetti <sagarnvs724@gmail.com>
2026-09-10 17:54:48 +0100

    confifured params for local floci container

--- infra_backup/main.tf
+++ infra_backup/main.tf
@@ -1,5 +1,18 @@
 provider "aws" {
   region = var.region
+
+  access_key = "test"
+  secret_key = "test"
+  
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
+
+  endpoints {
+    s3  = "http://127.0.0.1:4566"
+    sts = "http://127.0.0.1:4566"
+    iam = "http://127.0.0.1:4566"
+  }
 }
 
 resource "aws_s3_bucket" "tf_state" {
